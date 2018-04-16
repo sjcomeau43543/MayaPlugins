bb577ea7794a315f1ba37fc30cff7a1d5ddfcd6d
Matt <mjschueler@gmail.com>
2018-04-16 19:40:56 -0400

    more example gui code

--- gui_script.py
+++ gui_script.py
@@ -11,11 +11,44 @@ def getMayaWindow():
 class BasicDialog(QtWidgets.QDialog):
     def __init__(self, parent=getMayaWindow()):
         super(BasicDialog, self).__init__(parent)
-        self.setWindowTitle('blah')
-        self.shapeTypeCB = QtGui.QComboBox(parent=self)
-        self.nameLE = QtGui.QLineEdit('newShape', parent=self)
-        self.makeButton = QtGui.QPushButton("Make Shape", parent=self)
-        self.descLabel = QtGui.QLabel("This is a description", parent=self)
+        self.setWindowTitle('Maya PyQt Basic Dialog Demo')
+        self.shapeTypeCB = QtWidgets.QComboBox(parent=self)
+        self.nameLE = QtWidgets.QLineEdit('newShape', parent=self)
+        self.makeButton = QtWidgets.QPushButton("Make Shape", parent=self)
+        self.descLabel = QtWidgets.QLabel("This is a description", parent=self)
+        
+        self.shapeTypeCB.addItems(['Sphere', 'Cube', 'Cylinder'])
+        
+        self.connect(self.shapeTypeCB, QtCore.SIGNAL("currentIndexChanged(int)"), self.updateDescription)
+        self.connect(self.nameLE, QtCore.SIGNAL("textChanged(const QString&)"), self.updateDescription)
+        self.connect(self.makeButton, QtCore.SIGNAL("clicked()"), self.makeShape)
+        
+        actionLayout = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.LeftToRight)
+        actionLayout.addWidget(self.shapeTypeCB)
+        actionLayout.addWidget(self.nameLE)
+        actionLayout.addWidget(self.makeButton)
+        
+        self.updateDescription()
+
+        self.layout = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.TopToBottom, self)
+        self.layout.addLayout(actionLayout)
+        self.layout.addWidget(self.descLabel)
+    
+    def updateDescription(self):
+        description = 'Make a %s named "%s"' % (self.shapeTypeCB.currentText(), self.nameLE.text())
+        self.descLabel.setText(description)
+        
+    def makeShape(self):
+        objType = self.shapeTypeCB.currentText()
+
+        if objType == 'Sphere':
+            cmd = cmds.polySphere
+        elif objType == 'Cube':
+            cmd = cmds.polyCube
+        else:
+            cmd = cmds.polyCylinder
+
+        cmd(name=str(self.nameLE.text()))
 
 BasicDialog().show()
 
